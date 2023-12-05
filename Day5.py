5d21bcf9d2b36110db4b5eab989597764b76ce71
joybob22 <braydenlemke8@gmail.com>
2023-12-05 08:11:05 -0700

    made day 5 efficient

--- Day5.py
+++ Day5.py
@@ -11,10 +11,34 @@ class CustomRange():
     def __init__(self, lowerLimit, upperLimit) -> None:
         self.lowerLimit = lowerLimit
         self.upperLimit = upperLimit
+        self.changed = False
     
     def contains(self, num):
         # Range [lowerLimit, upperLimit)
         return num >= self.lowerLimit and num < self.upperLimit
+    
+    def rangeContainsRange(self, range):
+        check1 = (range.lowerLimit >= self.lowerLimit and range.lowerLimit < self.upperLimit) or (range.upperLimit <= self.upperLimit and range.upperLimit > self.lowerLimit)
+        check2 = (self.lowerLimit >= range.lowerLimit and self.lowerLimit < range.upperLimit) or (self.upperLimit <= range.upperLimit and self.upperLimit > range.lowerLimit)
+        return check1 or check2
+    
+    def splitOutOfRange(self, aRange):
+        # Assuming rangeContainsRange has been checked before this function call
+        newRanges = []
+        newRangeUpperLimit = aRange.upperLimit if aRange.upperLimit < self.upperLimit else self.upperLimit
+        newRangeLowerLimit = aRange.lowerLimit if aRange.lowerLimit > self.lowerLimit else self.lowerLimit
+        newRange = CustomRange(newRangeLowerLimit, newRangeUpperLimit)
+        newRange.changed = True
+        newRanges.append(newRange)
+        if newRange.lowerLimit > self.lowerLimit:
+            newRanges.append(CustomRange(self.lowerLimit, newRange.lowerLimit))
+        if newRange.upperLimit < self.upperLimit:
+            newRanges.append(CustomRange(newRange.upperLimit, self.upperLimit))
+        return newRanges
+    
+    def prettyString(self):
+        return str(self.lowerLimit) + "..<" + str(self.upperLimit)
+
 
 def part1():
     inputFileName = sys.argv[1]
@@ -63,12 +87,11 @@ def part2():
     seeds = sections.pop(0)
     pattern = r"\d+"
     seedNums = re.findall(pattern, seeds, re.M)
-    numsInQuestion = []
+    rangesInQuestion = []
 
     for i in range(int(len(seedNums) / 2)):
         pointer = i * 2
-        for j in range(int(seedNums[pointer+1])):
-            numsInQuestion.append(NumberContainer(int(seedNums[pointer]) + j))
+        rangesInQuestion.append(CustomRange(int(seedNums[pointer]), int(seedNums[pointer]) + int(seedNums[pointer+1])))
 
     pattern = r"^(\d+) (\d+) (\d+)$"
     for section in sections:
@@ -76,17 +99,27 @@ def part2():
         for destinationStart, sourceStart, rangeLength in nums:
             difference = int(destinationStart) - int(sourceStart)
             theRange = CustomRange(int(sourceStart), (int(sourceStart) + int(rangeLength)))
-            for numContainer in numsInQuestion:
-                if numContainer.changed == False and theRange.contains(numContainer.num):
-                    numContainer.num += difference
-                    numContainer.changed = True
-        for numContainer in numsInQuestion:
-            numContainer.changed = False
+            changedRangeIndexs = []
+            for (i, rangeInQuestion) in enumerate(rangesInQuestion):
+                if rangeInQuestion.changed == False and rangeInQuestion.rangeContainsRange(theRange):
+                    newRanges = rangeInQuestion.splitOutOfRange(theRange)
+                    for subRange in newRanges:
+                        if subRange.changed:
+                            subRange.upperLimit += difference
+                            subRange.lowerLimit += difference
+                        rangesInQuestion.append(subRange)
+                    changedRangeIndexs.append(i)
+            numRemovals = 0
+            for index in changedRangeIndexs:
+                rangesInQuestion.pop(index-numRemovals)
+                numRemovals+=1
+        for rangeContainer in rangesInQuestion:
+            rangeContainer.changed = False
 
-    smallestLocation = numsInQuestion.pop(0).num
-    for numContainer in numsInQuestion:
-        if numContainer.num < smallestLocation:
-            smallestLocation = numContainer.num
+    smallestLocation = rangesInQuestion.pop(0).lowerLimit
+    for subRange in rangesInQuestion:
+        if subRange.lowerLimit < smallestLocation:
+            smallestLocation = subRange.lowerLimit
     
     return smallestLocation
 
